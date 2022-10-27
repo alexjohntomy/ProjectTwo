9f6a98d9338b60c816b1cd705986ff82e54a95ff
Alex Tomy <atomy2@uic.edu>
2022-10-27 09:23:07 -0500

    literally everything

---
+++ js/03_functions.js
@@ -0,0 +1,11 @@
+function hoverChange_textcolor() { 
+    document.getElementById("last").style.opacity = 1;
+    console.log(window.document)
+    document.getElementById("last").innerHTML = message.value;
+}
+
+
+//Might use to set width of message boxes
+//var str = "Hello, world 123!";
+//var regex = /[a-zA-Z0-9]/g;
+//console.log(str.match(regex).length);

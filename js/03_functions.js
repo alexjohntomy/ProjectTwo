22b4486473fd73cff51c3d3f0595d1d70cfb273c
Alex Tomy <atomy2@uic.edu>
2022-11-07 23:05:05 -0600

    add conscience character, add styling elements, fix stuff

--- js/03_functions.js
+++ js/03_functions.js
@@ -1,3 +1,38 @@
+var count = -1;
+
+function fadeIn() {
+    document.body.style.opacity = 1;
+}
+
+function conscienceSequence() {
+
+    var hFour = document.createElement("H4");
+
+    //Define Array
+    var cArray = ["I guess it would be okay for me to tell him my name as long as it's just my name without any additional text", 
+    "No way, I need to ask him to tell me something only I would know",
+    "WOAH, I'm convinced. Need to ask him why he's texting me."];
+    
+    //forLoop
+    for (var i = 0; i < 30; i++) {
+    var conscienceArray = cArray[count];
+    }
+
+    function conscienceUpdate() {
+        document.getElementById('Conscience').appendChild(hFour);
+        document.getElementById('Conscience').innerHTML = '<h4> ༼∵༽ CONSCIENCE <br>' + conscienceArray + '</h4>';
+        document.getElementById('Conscience').style.color = "magenta";
+
+
+    }
+
+
+    setTimeout(conscienceUpdate, 800);
+
+}
+
+
+
 function sendMessage() { 
     //document.getElementById("Messages").style.opacity = 1;
     var h = document.createElement("H3");
@@ -5,24 +40,30 @@ function sendMessage() {
     h.appendChild(t);
     var DivGet = document.getElementById("Messages");
     DivGet.appendChild(h);
-    var nextTurn = true;
+    count++;
     setTimeout(aiResponse, 500);
 
 }
 
-
 function aiResponse() {
-    //document.getElementById("Messages").style.opacity = 1;
+
+    if (count==1) {
+        var name = message.value;
+        console.log(name);
+    }
+
     var hai = document.createElement("H2");
 
-
     //Define Array
-    var vArray = ["response1","response2","response3"];
+    var vArray = ["Hello World! I need to ensure I'm talking to the right person. What's your name?",
+    "Hi " + name + ", This is you from the future",
+    "Your glasses prescription is .75",
+    "I need your help... in the future, it's your invention of the internet that leads to WW3"
+    ];
     
     //forLoop
-    for (var i = 0; i < 3; i++) {
-    var arrayElement = vArray[i];
-    console.log(arrayElement);
+    for (var i = 0; i < 30; i++) {
+    var arrayElement = vArray[count];
     }
 
     //var tai = document.createTextNode(vArray[i]);
@@ -30,11 +71,4 @@ function aiResponse() {
     hai.appendChild(tai);
     var DivGet = document.getElementById("Messages");
     DivGet.appendChild(hai);
-    nextTurn = false;
 }
-
-
-//Might use to set width of message boxes
-//var str = "Hello, world 123!";
-//var regex = /[a-zA-Z0-9]/g;
-//console.log(str.match(regex).length);
